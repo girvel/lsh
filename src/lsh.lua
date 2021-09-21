bf7740b28e95853e384772d2265bbb39c7c7e06c
girvel <widauka@ya.ru>
2021-09-22 01:30:20 +0500

    Cover lsh.__call & lsh.__index w/ @documentation

--- src/lsh.lua
+++ src/lsh.lua
@@ -1,4 +1,4 @@
---- Tiny library encapsulating shell calls
+--- lsh (littleshell) - tiny library encapsulating shell calls
 -- Works like this:
 -- lsh 'cat .temp'
 -- lsh 'sudo aptitude update | sudo tee -a $APTITUDE_LOGS/update.txt'
@@ -16,12 +16,18 @@ local call = function(source)
 	return result
 end
 	
---- Executes shell command and returns output
+--- Executes shell command
+-- @param self lsh module
+-- @param command shell command
+-- @return command's output, exit code as a number
 mt.__call = function(self, command)	
 	return call(command), tonumber(call 'echo $?')
 end
 
---- Returns system environment variable's value
+--- Requests system environment variable's value
+-- @param self lsh module
+-- @param index variable's name
+-- @return variable's value
 mt.__index = function(self, index)
 	return call('echo $' .. index)
 end
